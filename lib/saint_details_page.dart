cbaadd1b680860010591b158c29cb95174ac69a5
alexeyismirnov <alexey@gmail.com>
2021-01-14 22:13:42 +0800

    Save icon to gallery

--- lib/saint_details_page.dart
+++ lib/saint_details_page.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
+import 'dart:async';
+import 'dart:io';
+import 'dart:core';
 
 import 'package:html2md/html2md.dart' as html2md;
 import 'package:esys_flutter_share/esys_flutter_share.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:gallery_saver/gallery_saver.dart';
+import 'package:path/path.dart' show join;
 
 import 'app_theme.dart';
 import 'db.dart';
@@ -21,17 +30,22 @@ class _SaintDetailPageState extends State<SaintDetailPage> {
   double _appBarHeight = 0.0;
 
   String markdown;
+  FToast fToast;
 
   @override
   void initState() {
     super.initState();
 
+    fToast = FToast();
+    fToast.init(context);
+
     markdown = html2md
-        .convert(widget.saint.zhitie)
-        .trim()
-        .replaceAll(RegExp(r'\n '), '\n')
-        .replaceAll('\\', '')
-        .replaceAll('\u00AD', '');
+            .convert(widget.saint.zhitie)
+            .trim()
+            .replaceAll(RegExp(r'\n '), '\n')
+            .replaceAll('\\', '')
+            .replaceAll('\u00AD', '') +
+        "\n";
 
     _scrollController = ScrollController()..addListener(() => setState(() {}));
   }
@@ -106,6 +120,47 @@ class _SaintDetailPageState extends State<SaintDetailPage> {
         });
   }
 
+  _showToast() {
+    Widget toast = Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25.0),
+        color: Colors.greenAccent,
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(Icons.check),
+          SizedBox(
+            width: 12.0,
+          ),
+          Text("Сохранено в Фотографиях"),
+        ],
+      ),
+    );
+
+    fToast.showToast(
+      child: toast,
+      gravity: ToastGravity.CENTER,
+      toastDuration: Duration(seconds: 2),
+    );
+  }
+
+  Future<File> getImageFileFromAssets(String path) async {
+    ByteData byteData = await rootBundle.load(join("icons", path));
+
+    final file = File('${(await getTemporaryDirectory()).path}/$path');
+    await file.writeAsBytes(byteData.buffer
+        .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+
+    return file;
+  }
+
+  Future<bool> saveIcon(String path) async {
+    File f = await getImageFileFromAssets(path);
+    return GallerySaver.saveImage(f.path);
+  }
+
   @override
   Widget build(BuildContext context) {
     final TabController controller = DefaultTabController.of(context);
@@ -158,13 +213,67 @@ class _SaintDetailPageState extends State<SaintDetailPage> {
                             mainAxisAlignment: MainAxisAlignment.start,
                             mainAxisSize: MainAxisSize.max,
                             children: <Widget>[
-                              widget.saint.has_icon
-                                  ? Material(
-                                      elevation: 10.0,
-                                      child: Image.asset(
-                                          "icons/${widget.saint.id}.jpg",
-                                          height: 280.0))
-                                  : Container(),
+                              if (widget.saint.has_icon)
+                                GestureDetector(
+                                    onTap: () {
+                                      AlertDialog alert = AlertDialog(
+                                          contentPadding: EdgeInsets.symmetric(
+                                              horizontal: 5.0, vertical: 10.0),
+                                          titlePadding: EdgeInsets.all(5.0),
+                                          title: Row(
+                                            mainAxisSize: MainAxisSize.max,
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              IconButton(
+                                                  icon: Icon(Icons.clear,
+                                                      size: 40.0),
+                                                  onPressed: () {
+                                                    Navigator.of(context).pop();
+                                                  }),
+                                              Expanded(
+                                                  child: Text(widget.saint.name,
+                                                      textAlign:
+                                                          TextAlign.center,
+                                                      maxLines: 2,
+                                                      overflow:
+                                                          TextOverflow.ellipsis,
+                                                      style: TextStyle(
+                                                        fontSize: 18.0,
+                                                      ))),
+                                              IconButton(
+                                                  icon: Icon(Icons.save,
+                                                      size: 40.0),
+                                                  onPressed: () {
+                                                    Navigator.of(context).pop();
+
+                                                    saveIcon(
+                                                            "${widget.saint.id}.jpg")
+                                                        .then((_) {
+                                                      _showToast();
+                                                    });
+                                                  })
+                                            ],
+                                          ),
+                                          content: Container(
+                                              width: 500.0,
+                                              height: 400.0,
+                                              child: FittedBox(
+                                                child: Image.asset(
+                                                  "icons/${widget.saint.id}.jpg",
+                                                ),
+                                                fit: BoxFit.contain,
+                                              )));
+                                      showDialog(
+                                          context: context,
+                                          builder: (BuildContext context) =>
+                                              alert);
+                                    },
+                                    child: Material(
+                                        elevation: 10.0,
+                                        child: Image.asset(
+                                            "icons/${widget.saint.id}.jpg",
+                                            height: 280.0))),
                               Container(
                                   padding: EdgeInsets.only(top: 10.0),
                                   constraints: BoxConstraints(
@@ -177,7 +286,6 @@ class _SaintDetailPageState extends State<SaintDetailPage> {
                                           overflow: TextOverflow.ellipsis,
                                           style: TextStyle(
                                             fontSize: 20.0,
-                                            //color: Colors.white
                                           ))))
                             ]),
                       ))),
